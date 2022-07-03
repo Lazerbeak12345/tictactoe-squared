e35cc52d350c8c1e4d2acfa356f5ac585f4f4576
Nathan Fritzler <nfblaster@live.com>
2022-07-03 14:59:01 -0600

    Remove fuffilled todo

--- src/ClickableCell.svelte
+++ src/ClickableCell.svelte
@@ -2,10 +2,6 @@
 	import Button from './Button.svelte'
 	export let disabled = false
 </script>
-<!-- TODO
-- States
- - Disabled
--->
 <Button classes="col m-1" {disabled}>
 	<slot>&nbsp;</slot>
 </Button>
