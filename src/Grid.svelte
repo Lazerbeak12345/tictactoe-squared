164a92661f2c9cc63ac82a0492f03c8f0b9392d1
Nathan Fritzler <nfblaster@live.com>
2022-07-03 16:17:29 -0600

    Add dynamic borders

--- src/Grid.svelte
+++ src/Grid.svelte
@@ -3,9 +3,9 @@
 	export let state
 	export let disabled = false
 </script>
-{#each state as row}
+{#each state as row, y}
 	<div class="row">
-		{#each row as cell}
+		{#each row as cell, x }
 			{#if typeof cell === "string"}
 				{#if cell === " "}
 					<ClickableCell {disabled}/>
@@ -15,7 +15,25 @@
 					</ClickableCell>
 				{/if}
 			{:else}
-				<div class="col border">
+				<div
+					class="col{
+						y < state.length - 1
+						? " border-bottom"
+						: " mb-1"
+					}{
+						y === 0
+						? " mt-1"
+						: ""
+					}{
+						x < row.length - 1
+						? " border-end"
+						: " me-1"
+					}{
+						x === 0
+						? " ms-1"
+						: ""
+					}"
+				>
 					<svelte:self state={cell} {disabled}/>
 				</div>
 			{/if}
