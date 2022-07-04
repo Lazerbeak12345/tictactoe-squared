e758d453abcd62e7aa3a03e44903b686a117c7bf
Nathan Fritzler <nfblaster@live.com>
2022-07-03 20:24:29 -0600

    Width and Height inputs

--- src/Gameboard.svelte
+++ src/Gameboard.svelte
@@ -5,20 +5,39 @@
 	let settings = true
 	let w = 3
 	let h = 3
-	let state = new Array(h)
+	$: state = new Array(h)
 		.fill(0)
 		.map(() => new Array(w)
 			.fill(0)
 			.map(() => " "))
-	console.log(state)
-	let maxMoves = w * h
+	$: maxMoves = w * h
 	let players = "XO"
 </script>
 <div class="card">
 	{#if settings}
 		<div class="card-body">
+			<label for="basic-url" class="form-label">
+				Size
+			</label>
+			<div class="input-group">
+				<span class="input-group-text" id="w">Width</span>
+				<input
+					type="number"
+					step="1"
+					class="form-control"
+					aria-describedby="w"
+					value="3"
+					on:change={e => w = parseInt(e.target.value)}>
+				<span class="input-group-text" id="h">Height</span>
+				<input
+					type="number"
+					step="1"
+					class="form-control"
+					aria-describedby="h"
+					value="3"
+					on:change={e => h = parseInt(e.target.value)}>
+			</div>
 			<ul>
-				<li>3 x 3 grid</li>
 				<li>2 Player, X and O</li>
 				<li>X goes first</li>
 			</ul>
