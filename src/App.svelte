aaf571694e28547cb49ed3a6741bdd3dff04d617
Nathan Fritzler <nfblaster@live.com>
2022-10-16 22:41:11 -0600

    Simplify parent, remove headless children

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,12 @@
 <script>
 	import { Button } from 'sveltestrap'
 
-	import Navbar from './Navbar.svelte'
-	import Gameboard from './Gameboard.svelte'
-
-	let settings = true
+	let a = true
 </script>
-<Navbar/>
-<div class="container-sm">
-	<div class="row justify-content-center">
-		<div class="col">
-			{#if settings}
-				<Button
-					on:click={()=>settings = false}
-				>
-					Play Game!
-				</Button>
-			{/if}
-		</div>
-	</div>
-</div>
-<div>scoreboard</div>
+{#if a}
+	<Button
+		on:click={()=>a = false}
+	>
+		Click me!
+	</Button>
+{/if}
--- src/Navbar.svelte
+++
@@ -1,8 +0,0 @@
-<nav class="navbar bg-primary mb-1">
-	<div class="container-fluid justify-content-center">
-		<h1 class="navbar-brand" href="#">Tic-Tac-Toe²</h1>
-		<small style="font: cursive, sans-serif" class="navbar-text"><em>
-			i got the name wrong. I'll fix it later
-		</em></small>
-	</div>
-</nav>
